6bbec3e1295b332039d63a6f9167fbf15c431281
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-31 22:11:38 +0530

    basurl change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portfolio-sumeetsingh/',
+  // base: '/portfolio-sumeetsingh/',
   plugins: [
     react(),
     tailwindcss(),
